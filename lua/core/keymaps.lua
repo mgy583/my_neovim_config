4b9d9a36231841b59ba96dc539fa158425f20e7c
mgy583 <2109767468@qq.com>
2025-07-13 17:25:12 +0800

    系统剪切板(wayland)

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,3 +13,24 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
+-- 设置剪贴板
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = "wl-copy --foreground --type text/plain",
+    ["*"] = "wl-copy --foreground --primary --type text/plain",
+  },
+  paste = {
+    ["+"] = function() return vim.fn.systemlist("wl-paste --no-newline", "") end,
+    ["*"] = function() return vim.fn.systemlist("wl-paste --no-newline --primary", "") end,
+  },
+  cache_enabled = true,
+}
+
+-- 关键映射
+vim.api.nvim_set_keymap("v", "y", '"+y', { noremap = true, silent = true, desc = "copy" })
+vim.api.nvim_set_keymap("n", "yy", '"+yy', { noremap = true, silent = true, desc = "copy" })
+
+vim.api.nvim_set_keymap("n", "p", '"+p', { noremap = true, silent = true, desc = "paste" })
+vim.api.nvim_set_keymap("v", "p", '"+p', { noremap = true, silent = true, desc = "paste" })
+
